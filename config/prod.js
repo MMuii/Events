63dfb1a40559f508c6e3fa5481f68f6444fe9356
mmui <muimuikrol@gmail.com>
2020-04-26 02:41:45 +0200

    Added heroku postbuild script

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://mmui:x012CMNkFo7NT30M@cluster0-nxcih.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'kluczdocookiesowjakijestkazdywidzi42!3%%@!43',
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 };
